350e7b109c26048c190ac58ccabfd5bab7698541
Alexandre Cédrick <alexandre.cedrick@gmail.com>
2020-12-02 18:09:08 +0100

    Add blog template

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -21,44 +21,46 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
 
+  theme: '@vuepress/theme-blog',
+
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
    *
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
-  themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
-    nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'Showroom',
-        link: '/showroom/'
-      }
-    ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
-      ],
-    }
-  },
+  // themeConfig: {
+  //   repo: '',
+  //   editLinks: false,
+  //   docsDir: '',
+  //   editLinkText: '',
+  //   lastUpdated: false,
+  //   nav: [
+  //     {
+  //       text: 'Guide',
+  //       link: '/guide/',
+  //     },
+  //     {
+  //       text: 'Config',
+  //       link: '/config/'
+  //     },
+  //     {
+  //       text: 'Showroom',
+  //       link: '/showroom/'
+  //     }
+  //   ],
+  //   sidebar: {
+  //     '/guide/': [
+  //       {
+  //         title: 'Guide',
+  //         collapsable: false,
+  //         children: [
+  //           '',
+  //           'using-vue',
+  //         ]
+  //       }
+  //     ],
+  //   }
+  // },
 
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
