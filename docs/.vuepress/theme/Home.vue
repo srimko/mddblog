11ceb7e85a4af9285cee6478c0563e0c0666de8d
Alexandre Cédrick <alexandre.cedrick@gmail.com>
2020-12-29 17:04:05 +0100

    Add circle and animations

--- docs/.vuepress/theme/Home.vue
+++ docs/.vuepress/theme/Home.vue
@@ -6,6 +6,8 @@
       <ArticleList />
     </main>
     <Footer />
+    <div class="bg-round round-1"></div>
+    <div class="bg-round round-2"></div>
     <!-- <Content  /> -->
     <!-- <div id="articles">
       <Content slot-key="articles" />
@@ -35,9 +37,65 @@ export default {
 
 
 <style lang="scss">
-main {
-  display: flex;
-  justify-content: space-between;
-  padding: 0 45px;
-}
+
+  .home {
+    min-height: 100vh;
+    position: relative;
+    overflow: hidden;
+  }
+
+  @keyframes move1 {
+    0% {
+      transform: translate(-30%, 0);
+    }
+    10% {
+      transform: translate(-10%, 0);
+    }
+    20% {
+      transform: translate(-13%, 10%);
+    }
+    100% {
+      transform: translate(-30%, 0%);
+    }
+  }
+  @keyframes move2 {
+    0% {
+      transform: translate(40%, 80%);
+    }
+    10% {
+      transform: translate(45%, 82%);
+    }
+    20% {
+      transform: translate(47%, 70%);
+    }
+    100% {
+      transform: translate(40%, 80%);
+    }
+  }
+
+  .bg-round {
+    position: absolute;
+    border-radius: 50%;
+    z-index: -1;
+
+    &.round-1 {
+      bottom: -50%;
+      width: 760px;
+      height: 760px;
+      transform: translate(-30%, 0);
+      background-color: rgba(87, 181, 206, 0.66);
+      filter: blur(35px);
+      animation: move1 5s 2;
+    }
+    &.round-2 {
+      top: -50%;
+      right: 0;
+      width: 500px;
+      height: 500px;
+      transform: translate(40%, 80%);
+      background-color: rgba(87, 206, 115, 0.56);
+      filter: blur(45px);
+      animation: move2 5s 2;
+    }
+  }
 </style>
